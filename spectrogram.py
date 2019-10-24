7051d7a94893866d3e2845595462c9ce7fcaa120
rheach17 <rheach17@gmail.com>
2019-10-24 16:43:36 +0530

    updated function

--- spectrogram.py
+++ spectrogram.py
@@ -1,16 +1,20 @@
 from math import log2
 from cmath import exp, pi
 from random import random
+import numpy as np
 
+#normal slow dft
 def dft(X):
     N = len(X)
     temp  = [0]*N
     for i in range(N):
         for k in range(N):
-            temp[i] += X[k] * exp(-2j*pi*k/N)
-    return temp
+            temp[i] += X[k] * exp(-2j*pi*i*k/N)
 
+    result = np.asarray(temp)
+    return result
 
+#cooley tukey for calculating dft(O(nlogn)) 
 def cooley_tukey(X):
     N = len(X)
     if N<=1:
@@ -20,16 +24,18 @@ def cooley_tukey(X):
 
     temp = [i for i in range(N)]
     for k in range(N//2):
-        temp[k] = even[k] + exp(-2.0j*pi*i*k/N)*odd[k]
-        temp[k+N//2] = even[k] - exp(-2.0j*pi*i*k/N)*odd[k]
-    return temp
+        temp[k] = even[k] + exp(-2.0j*pi*k/N)*odd[k]
+        temp[k+N//2] = even[k] - exp(-2.0j*pi*k/N)*odd[k]
+    result = np.asarray(temp)
+    return result
 
-'''X = []
-
-for i in range(32):
+#X = [1, 4, 3, 2, 2, 3, 4, 1]
+X = []
+for i in range(10):
     X.append(random())
 
 Y = cooley_tukey(X)
-print(Y)'''
-#T = dft(X)
+#print(Y)
+T = dft(X)
+#print(T)
 
